08ee1f298a30552185b79f2c281a2feab40276cf
Anna Elizabeth Woodard <awoodard@nd.edu>
2020-04-14 13:37:49 -0500

    Add option to run with Singularity

--- src/apps.py
+++ src/apps.py
@@ -54,34 +54,50 @@ def simulate_fusions(
         left_fq,
         right_fq,
         reads_per_sample,
-        image='olopadelab/fusion-simulation',
+        container_type='singularity',
         stderr=parsl.AUTO_LOGNAME,
         stdout=parsl.AUTO_LOGNAME):
     import os
-    os.makedirs(output_data, exist_ok=True)
 
-    command = (
-        'echo $HOSTNAME; '
-        'docker pull {image}; '
-        'docker run '
-        '--rm '
-        '-v {output_data}:/output_data '
-        '-v {gencode_annotation}:/gencode_annotation:ro '
-        '-v {reference_genome}:/reference_genome:ro '
-        '-v {reference_cdna}:/reference_cdna:ro '
-        '-v {left_fq}:/left_fq:ro '
-        '-v {right_fq}:/right_fq:ro '
-        '{image} '
+    command = ['echo $HOSTNAME; mkdir -p {output_data}; ']
+    if container_type == 'docker':
+        command += [
+            'docker run',
+            '--rm',
+            '-v {left_fq}:/left_fq:ro',
+            '-v {right_fq}:/right_fq:ro',
+            '-v {output_data}:/output_data ',
+            '-v {gencode_annotation}:/gencode_annotation:ro ',
+            '-v {reference_genome}:/reference_genome:ro ',
+            '-v {reference_cdna}:/reference_cdna:ro ',
+            'olopadelab/fusion-simulation:latest',
+        ]
+    elif container_type == 'singularity':
+        command += [
+            'singularity exec',
+            '-B {left_fq}:/left_fq',
+            '-B {right_fq}:/right_fq',
+            '-B {output_data}:/output_data',
+            '-B {gencode_annotation}:/gencode_annotation',
+            '-B {reference_genome}:/reference_genome',
+            '-B {reference_cdna}:/reference_cdna',
+            '{base_dir}/docker/fusion-simulation.sif'
+        ]
+    else:
+        raise RuntimeError('Container type must be either docker or singularity')
+
+    command += [
         'run.sh {fusions_per_sample} {reads_per_sample}'
-    )
-    return command.format(
+    ]
+
+    return ' '.join(command).format(
+        base_dir='/'.join(os.path.abspath(__file__).split('/')[:-2]),
         output_data=output_data,
         gencode_annotation=gencode_annotation,
         reference_genome=reference_genome,
         reference_cdna=reference_cdna,
         left_fq=left_fq,
         right_fq=right_fq,
-        image=image,
         fusions_per_sample=fusions_per_sample,
         reads_per_sample=reads_per_sample
     )
--- src/run.py
+++ src/run.py
@@ -20,42 +20,62 @@ parser.add_argument("--reference_cdna", help="")
 parser.add_argument("--reference_genome", help="")
 parser.add_argument("--fusions_per_sample", help="fusions to simulate per sample")
 parser.add_argument("--reads_per_sample", help="reads to simulate per sample")
-parser.add_argument("--replicates", type=int, help="reads to simulate per sample")
+# parser.add_argument("--replicates", type=int, help="reads to simulate per sample")
 parser.add_argument("--out_dir", default=None)
-
+parser.add_argument("--container_type", default='singularity')
+parser.add_argument("--config", default=None, help="Parsl config to parallelize with")
 args = parser.parse_args()
 
 base_dir = '/'.join(os.path.abspath(__file__).split('/')[:-2])
 if args.out_dir is None:
     args.out_dir = os.path.join(base_dir, 'data', 'processed')
+if args.config is None:
+    args.config = os.path.join(base_dir, 'fusionsimulation', 'configs', 'local.py')
+
+spec = importlib.util.spec_from_file_location('', args.config)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+parsl.load(module.config)
+
+if args.container_type == 'singularity':
+        image_path = '{base_dir}/docker/fusion-simulation.sif'.format(base_dir=base_dir)
+        # FIXME may require too much memory on some machines
+        if not os.path.isfile(image_path):
+            print('downloading {}'.format(image_path))
+            subprocess.call(
+                'singularity build {image_path} docker://olopadelab/fusion-simulation'.format(
+                    image_path=image_path),
+                shell=True
+            )
 
 sample_dirs = glob.glob(args.sample_dirs)
 
 for sample_dir in sample_dirs:
     print(sample_dir)
     sample_id = os.path.split(sample_dir)[-1]
-    for replicate in range(0, args.replicates):
-        output_data = os.path.join(args.out_dir, '{}_{}'.format(sample_id, replicate + 1))
-        print('submitting tasks for sample {}, replicate {}'.format(sample_id, replicate + 1))
+    # for replicate in range(0, args.replicates):
+    # output_data = os.path.join(args.out_dir, '{}_{}'.format(sample_id, replicate + 1))
+    output_data = os.path.join(args.out_dir, sample_id)
+    print('submitting tasks for sample {}'.format(sample_id))
 
-        left_fq = glob.glob(os.path.join(sample_dir, args.left))
-        right_fq = glob.glob(os.path.join(sample_dir, args.right))
-        if (len(left_fq) > 1) or (len(right_fq) > 1):
-            raise RuntimeError('Only one input fastq per sample supported!')
+    left_fq = glob.glob(os.path.join(sample_dir, args.left))
+    right_fq = glob.glob(os.path.join(sample_dir, args.right))
+    if (len(left_fq) > 1) or (len(right_fq) > 1):
+        raise RuntimeError('Only one input fastq per sample supported!')
 
-        simulate_fusions(
-            output_data,
-            args.gencode_annotation,
-            args.reference_genome,
-            args.reference_cdna,
-            args.fusions_per_sample,
-            left_fq[0],
-            right_fq[0],
-            args.reads_per_sample,
-            image='olopadelab/fusion-simulation',
-            stderr=parsl.AUTO_LOGNAME,
-            stdout=parsl.AUTO_LOGNAME
-        )
+    simulate_fusions(
+        output_data,
+        args.gencode_annotation,
+        args.reference_genome,
+        args.reference_cdna,
+        args.fusions_per_sample,
+        left_fq[0],
+        right_fq[0],
+        args.reads_per_sample,
+        container_type=args.container_type,
+        stderr=parsl.AUTO_LOGNAME,
+        stdout=parsl.AUTO_LOGNAME
+    )
 
 
 parsl.wait_for_current_tasks()
